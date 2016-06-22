7cc4bea7f463031bb5333fc36f2110e1013bb235
Andrija Petrovic <andrija.hers@gmail.com>
2016-06-22 11:15:03 +0000

    Attaching arrays of function now as well

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function createHookCollection(doublelinkedlistlib, inherit, isFunction){
+function createHookCollection(doublelinkedlistlib, inherit, isFunction, isArrayOfFunctions){
   var ListMixin = doublelinkedlistlib.Mixin,
     ListItemCtor = doublelinkedlistlib.Item;
 
-  function SingleShotSubscription (content) {
+  function SubscriptionBase (content) {
     if (!content) {
       console.trace();
       console.log('no function for SingleShotSubscription?', content);
@@ -11,29 +11,68 @@ function createHookCollection(doublelinkedlistlib, inherit, isFunction){
     }
     ListItemCtor.call(this, content);
   }
-  inherit(SingleShotSubscription, ListItemCtor);
-  SingleShotSubscription.prototype.apply = function (args) {
+  inherit(SubscriptionBase, ListItemCtor);
+  SubscriptionBase.prototype.apply = function (args) {
     if (!this.content) {
       console.trace();
       console.log(this);
       console.log(args);
       process.exit(0);
     }
-    this.content.apply(null, args);
+    this.doArgs(args);
   };
-  SingleShotSubscription.prototype.isSelfDestroyable = true;
 
-  function Subscription (list, content) {
-    SingleShotSubscription.call(this, content);
+  function SubscriptionMixin (list) {
     this.list = list;
   }
-  inherit(Subscription, SingleShotSubscription);
-  Subscription.prototype.destroy = function () {
+  SubscriptionMixin.prototype.destroy = function () {
     var l = this.list;
     this.list = null;
     if (l) {
       l.remove(this);
     }
+  };
+
+  function SingleShotSubscription (content) {
+    SubscriptionBase.call(this, content);
+  }
+  inherit(SingleShotSubscription, SubscriptionBase);
+  SingleShotSubscription.prototype.isSelfDestroyable = true;
+  SingleShotSubscription.prototype.doArgs = function (args) {
+    this.content.apply(null, args);
+  };
+
+  function Subscription (list, content) {
+    SingleShotSubscription.call(this, content);
+    SubscriptionMixin.call(this, list);
+  }
+  inherit(Subscription, SingleShotSubscription);
+  Subscription.prototype.destroy = function () {
+    SubscriptionMixin.prototype.destroy.call(this);
+    SingleShotSubscription.prototype.destroy.call(this);
+  };
+  Subscription.prototype.isSelfDestroyable = false;
+
+
+  function SingleShotSubscriptionOnArray (content) {
+    SubscriptionBase.call(this, content);
+  }
+  inherit(SingleShotSubscriptionOnArray, SubscriptionBase);
+  SingleShotSubscriptionOnArray.prototype.isSelfDestroyable = true;
+  SingleShotSubscriptionOnArray.prototype.doArgs = function (args) {
+    this.content.forEach(this.doSingleArgs.bind(this, args));
+  };
+  SingleShotSubscriptionOnArray.prototype.doSingleArgs = function (args, func) {
+    func.apply(null, args);
+  };
+
+  function SubscriptionOnArray (list, content) {
+    SingleShotSubscriptionOnArray.call(this, content);
+    SubscriptionMixin.call(this, list);
+  }
+  inherit(SubscriptionOnArray, SingleShotSubscriptionOnArray);
+  SubscriptionOnArray.prototype.destroy = function () {
+    SubscriptionMixin.prototype.destroy.call(this);
     SingleShotSubscription.prototype.destroy.call(this);
   };
   Subscription.prototype.isSelfDestroyable = false;
@@ -46,20 +85,30 @@ function createHookCollection(doublelinkedlistlib, inherit, isFunction){
   HookCollection.prototype.destruct = HookCollection.prototype.destroy;
   HookCollection.prototype.attach = function (cb) {
     var item;
-    if (!isFunction(cb)) {
-      return;
+    if (isFunction(cb)) {// || isArrayOfFunctions(cb))) {
+      item = new Subscription(this, cb);
     }
-    item = new Subscription(this, cb);
+    if (isArrayOfFunctions(cb)) {
+      item = new SubscriptionOnArray(this, cb);
+    }
+    if (!item) {
+      throw new Error('Cannot attach');
+    }
     this.assureForController();
     this.controller.addToBack(item);
     return item;
   };
   HookCollection.prototype.attachForSingleShot = function (cb) {
     var item;
-    if (!isFunction(cb)) {
-      return;
+    if (isFunction(cb)) {
+      item = new SingleShotSubscription(cb);
     }
-    item = new SingleShotSubscription(cb);
+    if (isArrayOfFunctions(cb)) {
+      item = new SingleShotSubscriptionOnArray(cb);
+    }
+    if (!item) {
+      throw new Error('Cannot attach');
+    }
     this.assureForController();
     this.controller.addToBack(item);
   };
